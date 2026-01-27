7c2c3a311811644f17e1081a94f664d6e084fc20
Sakshi-HC-Gowda <sakshicgowd@gmail.com>
2026-01-27 21:34:58 +0530

    Create Day6_Strings.java

---
+++ Day6_Strings.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int T = sc.nextInt();   
+        sc.nextLine(); 
+        for (int t = 0; t < T; t++) {
+            String s = sc.nextLine();
+            StringBuilder even = new StringBuilder();
+            StringBuilder odd = new StringBuilder();
+
+            for (int i = 0; i < s.length(); i++) {
+                if (i % 2 == 0) {
+                    even.append(s.charAt(i));
+                } else {
+                    odd.append(s.charAt(i));
+                }
+            }
+
+            System.out.println(even + " " + odd);
+        }
+    }
+}
+
+
+
+
