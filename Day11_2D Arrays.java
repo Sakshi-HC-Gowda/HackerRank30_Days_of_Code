65f6e093ee283d856bf9a1ef746296166a428764
Sakshi-HC-Gowda <sakshicgowd@gmail.com>
2026-02-02 17:03:42 +0530

    Implement hourglass sum calculation in 2D array

---
+++ Day11_2D Arrays.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        List<List<Integer>> arr = new ArrayList<>();
+
+        // Read 6x6 list
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowTempItems = bufferedReader.readLine().trim().split(" ");
+            List<Integer> arrRowItems = new ArrayList<>();
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowTempItems[j]);
+                arrRowItems.add(arrItem);
+            }
+            arr.add(arrRowItems);
+        }
+        bufferedReader.close();
+        int maxSum = Integer.MIN_VALUE;
+        // Compute hourglass sums
+        for (int i = 0; i <= 3; i++) {
+            for (int j = 0; j <= 3; j++) {
+                int sum = arr.get(i).get(j) + arr.get(i).get(j+1) + arr.get(i).get(j+2)
+                        + arr.get(i+1).get(j+1)
+                        + arr.get(i+2).get(j) + arr.get(i+2).get(j+1) + arr.get(i+2).get(j+2);
+
+                if (sum > maxSum) {
+                    maxSum = sum;
+                }
+            }
+        }
+        System.out.println(maxSum);
+    }
+}
